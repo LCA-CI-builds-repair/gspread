379d8d2977a307e64c41c6217d470774b1b86cea
ubuntu <ubuntu@ip-10-0-1-33>
2025-01-16 01:45:16 +0000

    waleko__claude-3.5-sonnet_a930__id_138

--- gspread/worksheet.py
+++ gspread/worksheet.py
@@ -143,7 +143,10 @@ class ValueRange(list):
         If the range is empty, return the default value.
         """
         try:
-            return self[0][0]
+            if isinstance(self, list):
+                return self[0][0] if self and self[0] else default
+            # When self is ValueRange
+            return self[0][0] if len(self) > 0 and len(self[0]) > 0 else default
         except IndexError:
             return default
 
